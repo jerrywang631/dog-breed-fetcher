da60be54cc30e9a7dadc57b0707f8db2d062c84c
Jerry <jerrywang631@gmail.com>
2025-10-28 09:28:40 +0800

    complete the dog api breed class for last commit, this is for the caching breed fetcher

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,19 +13,38 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    private final BreedFetcher underlyingFetcher;
+    private final Map<String, List<String>> cache = new HashMap<>();
     private int callsMade = 0;
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+            this.underlyingFetcher = fetcher;
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        if (cache.containsKey(breed.toLowerCase())) {
+            return cache.get(breed.toLowerCase());
+        }
+
+        try {
+            // 调用底层 fetcher
+            List<String> result = underlyingFetcher.getSubBreeds(breed);
+            callsMade++;
+
+            // 存入缓存（仅成功结果缓存）
+            cache.put(breed.toLowerCase(), result);
+            return result;
+
+        } catch (BreedNotFoundException e) {
+            // 不缓存失败结果
+            callsMade++;
+            throw e;
+        }
     }
 
     public int getCallsMade() {
         return callsMade;
     }
-}
+    }
+
+
